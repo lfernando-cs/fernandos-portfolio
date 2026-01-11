be88bcb1324794d9c9ef1bfc9c6243595962e577
lfernando-cs <fwzk777@gmail.com>
2026-01-10 22:32:35 -0400

    cambios generales en la estructura del proyecto

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,3 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
-
-export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+export async function GET() {
+  return new Response("Not Found", { status: 404 });
 }
